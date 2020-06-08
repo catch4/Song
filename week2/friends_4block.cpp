13a4630b8432697fa5812de604efdf44818f3285
Songminkee <thdalsrl10@gmail.com>
2020-06-08 19:43:54 +0900

    friends4block

--- week2/friends_4block.cpp
+++ week2/friends_4block.cpp
@@ -43,20 +43,6 @@ void pang(std::vector<string>& map, std::vector<std::vector<bool>>& ck)
 		}
 	}
 
-	std::cout << "ck" << std::endl;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++)
-			std::cout << ck[i][j];
-		std::cout << '\n';
-	}
-
-	std::cout << "map" << std::endl;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++)
-			std::cout << map[i][j];
-		std::cout << '\n';
-	}
-	std::cout << '\n';
 }
 
 // 인접 2x2 찾는 부분
@@ -103,13 +89,3 @@ int solution(int c_n, int c_m, vector<string> map) {
 	return answer;
 }
 
-int main()
-{
-	std::cin >> n >> m;
-	std::vector<string> map(n);
-	bool flag = true;
-	for (int i = 0; i < n; i++)
-		std::cin >> map[i];
-	std::cout << solution(n,m,map) << std::endl;
-	return 0;
-}
