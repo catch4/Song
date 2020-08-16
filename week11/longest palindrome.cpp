04cc5de81bc9edd795b70397aad620dbc8e10a07
Songminkee <thdalsrl10@gmail.com>
2020-08-16 21:30:36 +0900

    week11 programmers 12904

---
+++ week11/longest palindrome.cpp
@@ -0,0 +1,45 @@
+/*
+Catch study 11주차
+프로그래머스_가장 긴 팰린드롬
+https://programmers.co.kr/learn/courses/30/lessons/12904
+*/
+/*-----------------
+풀이
+현재 만들어진 최대값보다 작을 경우 함수에서 나온다.
+-----------------*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int palin(string& str,const int& cur, int limit) {
+	int n_cur = str.size() - cur;
+	while (n_cur>limit) {
+		bool flag = 1;
+		for (int i = 0; i < (n_cur + 1) / 2; i++)
+		{
+			if (str[cur + i] != str[cur + n_cur - 1 - i])
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag)
+			return n_cur;
+		else
+			n_cur -= 1;
+	}
+	return limit;
+}
+int solution(string s)
+{
+	int answer = 1;
+	for (int i = 0; i < s.size(); i++)
+		answer = palin(s, i, answer);
+	return answer;
+}
+
+int main() {
+	std::string s = "abcdcba";
+	//std::string s = "abacde";
+	std::cout << solution(s);
+}
--- week11/readme.md
+++ week11/readme.md
@@ -4,6 +4,8 @@
 
   - ##### [프로그래머스_[1차 캐시]](https://programmers.co.kr/learn/courses/30/lessons/17680?language=cpp) - [[풀이](https://github.com/catch4/Song/blob/master/week11/cache.cpp)]
   
+  - ##### [프로그래머스_가장 긴 팰린드롬](https://programmers.co.kr/learn/courses/30/lessons/12904) - [[풀이](https://github.com/catch4/Song/blob/master/week11/longest palindrome.cpp)]
+  
 - #### 백준
 
   - ##### [백준_15999 뒤집기](https://www.acmicpc.net/problem/15999) - [[풀이](https://github.com/catch4/Song/blob/master/week11/15999_reverse.cpp)]
