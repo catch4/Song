6f258d165f00d74b851e750416e3e4924f89cddf
Songminkee <thdalsrl10@gmail.com>
2020-09-28 22:13:09 +0900

    week17 baekjoon 17142 retry

--- week17/17142_lab3.cpp
+++ week17/17142_lab3.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 #include <cstring>
 #define pi std::pair<int,int>
-int map[50][50],ch[10],mp[50][50];
+int map[50][50], ch[10], mp[50][50];
 int answer = 2147000000, n, m, dy[4] = { 1,0,-1,0 }, dx[4] = { 0,1,0,-1 }, zero = 0;
 
 std::vector<pi> virus;
@@ -23,39 +24,40 @@ void bfs() {
 		Q.pop();
 		for (int k = 0; k < 4; k++) {
 			int ny = now.first + dy[k], nx = now.second + dx[k];
-			if (ny < 0 || ny >= n || nx < 0 || nx >= n || mp[ny][nx] != -1 || map[ny][nx]) continue;
+			if (ny < 0 || ny >= n || nx < 0 || nx >= n || mp[ny][nx] != -1 || map[ny][nx] == 1) continue;
+
 			mp[ny][nx] = mp[now.first][now.second] + 1;
 			Q.push({ ny,nx });
-			cnt++;
-			if (max<mp[ny][nx])
-				max = mp[ny][nx];
-			else if (max >= answer) return;
+			if (map[ny][nx] != 2) {
+				cnt++;
+				if (max<mp[ny][nx])
+					max = mp[ny][nx];
+				else if (max >= answer) return;
+			}
 		}
 	}
 	if (cnt != zero) return;
-	if (max < answer)
-		answer = max;
+	answer = std::min(max, answer);
 }
 
-void dfs(const int& idx) {
+void dfs(const int& idx, const int& pre) {
 	if (idx == m)
 		bfs();
 	else {
-		for (int i = 0; i < virus.size(); i++)
+		for (int i = pre; i < virus.size(); i++)
 		{
-			if (!ck[i]) {
-				ck[i] = 1;
-				ch[idx] = i;
-				dfs(idx + 1);
-				ck[i] = 0;
-			}
+			if (ck[i]) continue;
+			ck[i] = 1;
+			ch[idx] = i;
+			dfs(idx + 1, i);
+			ck[i] = 0;
 		}
 	}
 }
 
 int main() {
-	std::cin >> n >> m;
 	std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
+	std::cin >> n >> m;
 
 	for (int i = 0; i<n; i++)
 		for (int j = 0; j < n; j++) {
@@ -72,7 +74,7 @@ int main() {
 		return 0;
 	}
 	ck.resize(virus.size());
-	dfs(0);
+	dfs(0, 0);
 	if (answer == 2147000000) std::cout << -1 << '\n';
 	else std::cout << answer << '\n';
 	return 0;
