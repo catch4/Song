81b9404deeabe991e15c0d960926e72f9aa4d500
songminkee <thdalsrl10@gmail.com>
2020-10-05 23:32:23 +0900

    week18 baekjoon 14226 retry

--- week18/14226.cpp
+++ week18/14226.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
+
+struct Info{
+	int t,n,c;
+};
 
 int main() {
 	int dst;
 	std::cin >> dst;
-	std::vector<int> vec(dst + 1);
-	for (int i = 2; i <= dst; i++) vec[i] = i;
-	for (int i = 2; i <= dst; i++) {			
-		int t = vec[i] + 1;
-		for (int j = i; j <= dst; j += i) {
-			vec[j] = std::min(t,vec[j]);
-			t++;
+	std::vector<std::vector<bool>> ck(dst+1,std::vector<bool>(dst+1,0));
+	std::queue<Info> Q;
+	Q.push({0,1,0});
+	while(!Q.empty()){
+		Info now = Q.front();
+		Q.pop();
+		if(now.n==dst){
+			std::cout<<now.t<<'\n';
+			return 0;
 		}
-		for (int j = dst - 1; j >= 2; j--) {
-			vec[j] = std::min(vec[j], vec[j + 1] + 1);
+		if(now.c){
+			if(now.n+now.c<=dst &&!ck[now.n+now.c][now.c]){
+				ck[now.n+now.c][now.c]=1;
+				Q.push({now.t+1,now.n+now.c,now.c});
+			}
+		}
+		if(now.n>1 &&! ck[now.n-1][now.c])
+		{
+			ck[now.n-1][now.c]=1;
+			Q.push({now.t+1,now.n-1,now.c});
+		}
+		if(!ck[now.n][now.n]){
+			ck[now.n][now.n]=1;
+			Q.push({now.t+1,now.n,now.n});
 		}
 	}
-
-	std::cout << vec[dst] << '\n';
-	
-	return 0;
 }
